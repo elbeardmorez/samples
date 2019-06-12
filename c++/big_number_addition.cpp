06ed0212ef9e4fc0edbb0fbc360f8782cc100bc1
Pete Beardmore <pete.beardmore@msn.com>
2019-06-12 16:01:53 +0100

    [mod] c++, big number addition
    -switch to 'stoi' from 'atoi'

--- c++/big_number_addition.cpp
+++ c++/big_number_addition.cpp
@@ -22,10 +22,10 @@ void add_(string s1, string s2, string& res) {
     string sum;
     //cout << "adding s1: " << s1 << ", s2: " << s2 << ", l_s1: " << l_s1 << ", l_s2: " << l_s2 << endl;
     while (idx < l_s1) {
-        i1 = atoi(s1.substr(l_s1 - idx - 1, 1).c_str());
+        i1 = stoi(s1.substr(l_s1 - idx - 1, 1));
         i2 = 0;
         if (idx < l_s2)
-            i2 = atoi(s2.substr(l_s2 - idx - 1, 1).c_str());
+            i2 = stoi(s2.substr(l_s2 - idx - 1, 1));
         //cout << "adding i1: " << i1 << ", i2: " << i2 << endl;
         sum = to_string(i1 + i2 + carry);
         if (sum.length() == 1) {
@@ -33,7 +33,7 @@ void add_(string s1, string s2, string& res) {
             res = sum + res;
         } else {
             res = sum.substr(sum.length() - 1) + res;
-            carry = atoi(sum.substr(0, sum.length() -1).c_str());
+            carry = stoi(sum.substr(0, sum.length() - 1));
         }
         idx++;
     }
