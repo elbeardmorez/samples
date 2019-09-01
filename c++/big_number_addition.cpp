58a5718fbd31f3c0360901f90dfea9722a254a6f
Pete Beardmore <pete.beardmore@msn.com>
2019-09-01 19:02:59 +0100

    [mod] c++, big number addition
    -support decimals

--- c++/big_number_addition.cpp
+++ c++/big_number_addition.cpp
@@ -7,6 +7,9 @@ using namespace std;
 
 void add_(string s1, string s2, string& res) {
 
+    int l_s1 = s1.length();
+    int l_s2 = s2.length();
+
     // operator
     std::function<int(int, int)> op;
     int n1 = 0;
@@ -14,23 +17,52 @@ void add_(string s1, string s2, string& res) {
     if (s1.substr(0, 1) == "-") {
         n1 = 1;
         s1 = s1.substr(1);
+        l_s1--;
     }
     if (s2.substr(0, 1) == "-") {
         n2 = 1;
         s2 = s2.substr(1);
+        l_s2--;
     }
     if (n1 + n2 == 1)
         op = std::minus<int>();
     else
         op = std::plus<int>();
 
+    // equalise mantissa
+    int dot1 = s1.find(".");
+    int dot2 = s2.find(".");
+    if (dot1 == -1) {
+        s1.push_back('.');
+        s1.push_back('0');
+        l_s1 += 2;
+        dot1 = l_s1 - 2;
+    }
+    if (dot2 == -1) {
+        s2.push_back('.');
+        s2.push_back('0');
+        l_s2 += 2;
+        dot2 = l_s2 - 2;
+    }
+    string *x;
+    int append;
+    if (l_s1 - dot1 < l_s2 - dot2) {
+        x = &s1;
+        append = (l_s2 - dot2) - (l_s1 - dot1);
+        l_s1 += append;
+    } else if (l_s1 - dot1 > l_s2 - dot2) {
+        x = &s2;
+        append = (l_s1 - dot1) - (l_s2 - dot2);
+        l_s2 += append;
+    }
+    for (int l = 0; l < append; l++) x->push_back('0');
+
     // order by magnitude
-    int l_s1 = s1.length();
-    int l_s2 = s2.length();
     int switch_ = 0;
     if (l_s1 == l_s2) {
         for (int l = 0; l < l_s1; l++) {
-            if (stoi(s1.substr(l, 1)) < stoi(s1.substr(l, 1))) {
+            if (s1.substr(l, 1) == ".") continue;
+            if (stoi(s1.substr(l, 1)) < stoi(s2.substr(l, 1))) {
                 switch_ = 1;
                 break;
             }
@@ -64,6 +96,11 @@ void add_(string s1, string s2, string& res) {
     //cout << "adding s1: " << s1 << ", s2: " << s2 << ", l_s1: " << l_s1 << ", l_s2: " << l_s2 << endl;
     while (idx < l_s1) {
         ss1 = s1.substr(l_s1 - idx - 1, 1);
+        if (ss1 == ".") {
+            res = "." + res;
+            idx++;
+            continue;
+        }
         i1 = stoi(ss1);
         i2 = 0;
         if (idx < l_s2) {
