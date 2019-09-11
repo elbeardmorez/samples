0a8263967bb435175f0787cc0772ee6df94c902a
Pete Beardmore <pete.beardmore@msn.com>
2019-09-11 14:53:05 +0100

    [add] c++, groupby, support pointers as keys
    -add additional variadic template arg to support optional map key
    comparator argument (a predicate / bool function) required for
    useful pointer comparison (i.e. passing a function reference to
    test the underlying type as opposed to the arbitrary / random pointer
    value)

--- c++/groupby.cpp
+++ c++/groupby.cpp
@@ -18,13 +18,19 @@ class obj {
         }
 };
 
-template <typename X, typename K, typename V>
-void groupby(X input, map<K, vector<V>>& groups, K (*f)(V) = [](K x){return x;}) {
+template <typename I, typename K, typename V, typename... Mx>
+void groupby(I input, map<K, vector<V>, Mx...> &groups, K (*f)(V&) = [](V& x) -> K { return x; }) {
     for (auto itr = input.begin(); itr != input.end(); ++itr) {
         K k = f(*itr);
-        if (groups.find(k) == groups.end())
-            groups[k].reserve(1);
-        groups[k].push_back(*itr);
+        auto itr2 = groups.find(k);
+        if (itr2 == groups.end()) {
+            //cout << "adding new key: '" << k << "'" << endl;
+            groups[k] = vector<V> {*itr};
+        } else {
+            //cout << "searched for k: " << k << ", found: " << itr2->first << endl;
+            //cout << "appending to existing key: '" << k << "'" << endl;
+            itr2->second.emplace_back(*itr);
+        }
     }
 }
 
@@ -34,7 +40,7 @@ void tests() {
     string s = "tiesto";
     map<char, vector<char>> map_;
     groupby(s, map_);
-    cout << "test: '" << s << "'";
+    cout << "testing string: '" << s << "'";
     for (auto itr = map_.begin(); itr != map_.end(); ++itr) cout << "k: " << itr->first << ", [v]: " << itr->second.size() << endl;
 
     // objects
@@ -43,13 +49,23 @@ void tests() {
     vector<obj> objs(4);
     for (int l = 0; l < objs.size(); l++) objs[l] = obj(s2[l], i2[l]);
     map<string, vector<obj>> map_2;
-    string (*f_key)(obj) = [](obj x) { return x.name; };
+    string (*f_key)(obj&) = [](obj& x) { return x.name; };
     groupby<vector<obj>, string, obj>(objs, map_2, f_key);
-    cout << "test:";
+    cout << "testing objects:";
     for (auto itr = objs.begin(); itr != objs.end(); ++itr)
         cout << " " << itr->name << "|" << itr->size;
     cout << endl;
     for (auto itr = map_2.begin(); itr != map_2.end(); ++itr)
+        cout << "k: " << itr->first << ", [v]: " << itr->second.size() << endl;
+
+    // pointer keys
+    vector<const char*> v{"alpha", "beta", "charlie", "alpha"};
+    map<const char *, vector<const char*>, bool (*)(const char*, const char*)> map_3([](const char* x, const char* y) -> bool { return strcmp(x, y) < 0; });
+    groupby<vector<const char*>, const char*>(v, map_3);
+    //groupby(v, map_3);
+    cout << "testing pointer keys:";
+    for (auto itr = v.begin(); itr != v.end(); ++itr) cout << " " << *itr; cout << endl;
+    for (auto itr = map_3.begin(); itr != map_3.end(); ++itr)
         cout << "k: " << itr->first << ", [v]: " << itr->second.size() << endl;
 }
 
@@ -74,5 +90,7 @@ int main(int argc, char** argv) {
 // explicitly instantiate templates for debugging
 template class std::vector<char>;
 template class std::vector<obj>;
-template class std::map<char, vector<char>>;
+template class std::vector<const char*>;
 template class std::map<string, vector<obj>>;
+template class std::map<char, vector<obj>>;
+template class std::map<const char*, vector<const char*>>;
